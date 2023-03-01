785d2135396c38c5eee65e9ef41b16f1e0ccf68a
wasififtikhar <wasifadif@gmail.com>
2023-03-01 14:12:53 +0500

    code formating and cleaning

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 //Wrap code in an IIFE
-(function(){
-  
-  let screen = document.querySelector('.screen');
-  let buttons = document.querySelectorAll('.btn');
-  let clear = document.querySelector('.btn-clear');
-  let equal = document.querySelector('.btn-equal');
-  
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
   //retrieve data from numbers that are clicked
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
       let value = e.target.dataset.num;
       screen.value += value;
-    })
+    });
   });
-  
-  equal.addEventListener('click', function(e){
-    if(screen.value === ''){
-      screen.value = 'Please Enter a Value';
+
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "Please Enter a Value";
     } else {
       let answer = eval(screen.value);
       screen.value = answer;
     }
-  })
-  
-  clear.addEventListener('click', function(e){
-    screen.value = '';
-  })
- 
+  });
+
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
 })(); //end IIFE
